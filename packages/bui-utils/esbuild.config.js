c9e603bcd94b5cd3e5266a7ee1f131a8298107b6
whyrove <whyrove@163.com>
2024-09-13 19:27:09 +0800

    feat: 替换构建工具

---
+++ packages/bui-utils/esbuild.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const fs = require('fs-extra');
+const path = require('path');
+const esBuilder = require('../../esbuild.base.ts');
+
+const distPath = path.join(__dirname, 'dist');
+if (fs.existsSync(distPath)) {
+  fs.removeSync(distPath);
+}
+const esPath = path.join(__dirname, 'es');
+if (fs.existsSync(esPath)) {
+  fs.removeSync(esPath);
+}
+
+esBuilder(['./src/**/*.ts']);
