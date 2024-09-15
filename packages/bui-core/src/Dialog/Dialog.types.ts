28f6c777cbac60acc8ac148c0861365b020532d2
jellyshuang <1394756525@qq.com>
2024-09-15 16:45:00 +0800

    feat: add Dialog component (80%)

--- packages/bui-core/src/Dialog/Dialog.types.ts
+++ packages/bui-core/src/Dialog/Dialog.types.ts
@@ -3,30 +3,62 @@ import { ModalProps } from '../Modal/Modal.types';
 
 export type Action = boolean | string | number;
 export type Dispatch = (action: Action, val?: string) => void;
+/**
+ * 对话框类型
+ */
 export type DialogType = 'confirm' | 'prompt';
 export type Render = ReactNode | ((dispatch: Dispatch) => ReactNode);
-export type CloseBefore = (action: Action) => Promise<any> | void;
-
-type HandleEvent = () => void;
 
 export interface DialogProps extends ModalProps {
+  /**
+   * 对话框类型
+   * @default confirm
+   */
   type?: DialogType;
-
+  /**
+   * 自定义文本区域内容
+   */
   custom?: Render;
+  /**
+   * 自定义标题
+   */
   header?: Render;
+  /**
+   * 自定义内容
+   */
   desc?: Render;
+  /**
+   * 自定义按钮区域
+   */
   footer?: Render;
-
+  /**
+   * 是否显示
+   */
   visible?: boolean;
+  /**
+   * 输入框占位文本
+   */
   placeholder?: string;
+  /**
+   * 执行操作
+   */
   dispatch?: Dispatch;
-
+  /**
+   * 确认文本内容
+   */
   confirmText?: string;
+  /**
+   * 取消文本内容
+   */
   cancelText?: string;
+  /**
+   * 确认回调
+   */
   onConfirm?: (val?: string) => void | Promise<void | string>;
+  /**
+   * 取消回调
+   */
   onCancel?: () => void | Promise<void>;
-  closeBefore?: CloseBefore;
-  closeAfter?: HandleEvent;
 }
 
 /**
@@ -34,23 +66,26 @@ export interface DialogProps extends ModalProps {
  */
 export type DialogOptions = Omit<DialogProps, 'type'> | string;
 
+/**
+ * prompt函数式调用配置参数
+ */
 export type PromptOptions = DialogOptions & {
   placeholder?: string;
   type?: string;
 };
 
-export type ConfirmOptions = DialogOptions & {
-  placeholder?: string;
-  type?: string;
-};
-
-// export type Params = DialogProps | ReactNode;
-// export type PromptParams = PromptProps | ReactNode;
+/**
+ * confirm函数式调用配置参数
+ */
+export type ConfirmOptions = DialogOptions;
+/**
+ * Dialog函数式调用返回值类型
+ */
 export type DialogPromise = Promise<boolean | string>;
 export type Options = PromptOptions | ConfirmOptions;
 
 /**
- * Toast Instance
+ * Dialog Instance
  */
 export interface DialogInstance {
   (options: Options): DialogPromise;
