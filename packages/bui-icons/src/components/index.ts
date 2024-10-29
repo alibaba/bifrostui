70ce2c5a27e1b337ea240963dbaa6516e3b9b04e
whyrove <whyrove@163.com>
2024-10-29 15:47:08 +0800

    feat: rename icon

--- packages/bui-icons/src/components/index.ts
+++ packages/bui-icons/src/components/index.ts
@@ -130,6 +130,8 @@ export { default as LoadingThreeQuartersOutlinedIcon } from './LoadingThreeQuart
 export { default as LocationFilledIcon } from './LocationFilled';
 export { default as LocationOutlinedIcon } from './LocationOutlined';
 export { default as LuckyMoneyOutlinedIcon } from './LuckyMoneyOutlined';
+export { default as MaiSuiLeftIcon } from './MaiSuiLeft';
+export { default as MaiSuiRightIcon } from './MaiSuiRight';
 export { default as MailOutlinedIcon } from './MailOutlined';
 export { default as MaleOutlinedIcon } from './MaleOutlined';
 export { default as MapFilledIcon } from './MapFilled';
@@ -231,5 +233,3 @@ export { default as WifiOutlinedIcon } from './WifiOutlined';
 export { default as XiaMiCircleOutlinedIcon } from './XiaMiCircleOutlined';
 export { default as YouKuVipCircleOutlinedIcon } from './YouKuVipCircleOutlined';
 export { default as ZoomInOutlinedIcon } from './ZoomInOutlined';
-export { default as maisuiyouIcon } from './maisuiyou';
-export { default as maisuizuoIcon } from './maisuizuo';
