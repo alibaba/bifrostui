4a8221fae2b4455ee1284449dcc3f13b9bce4c06
kwzm <kwzm93@163.com>
2024-11-05 17:34:36 +0800

    feat: add CollapsePanel component (80%)

--- packages/bui-core/src/CollapsePanel/CollapsePanelItem.tsx
+++ packages/bui-core/src/CollapsePanel/CollapsePanelItem.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import clsx from 'clsx';
+import { Collapse } from '../Collapse';
 import { CollapsePanelItemProps } from './CollapsePanel.types';
 
 const prefixCls = 'bui-collapse-panel';
 
-const CollapsePanelItem = (props: CollapsePanelItemProps) => {
-  const { className, style, label, children, icon, active, onClick } = props;
+const CollapsePanelItem = React.forwardRef<
+  HTMLDivElement,
+  CollapsePanelItemProps
+>((props, ref) => {
+  const {
+    className,
+    style,
+    label,
+    children,
+    icon,
+    active,
+    onClick,
+    ...others
+  } = props;
   const isControlledIcon = typeof icon === 'function';
 
   const getIcon = () => {
@@ -13,7 +26,12 @@ const CollapsePanelItem = (props: CollapsePanelItemProps) => {
   };
 
   return (
-    <div className={clsx(`${prefixCls}-item`, className)} style={style}>
+    <div
+      className={clsx(`${prefixCls}-item`, className)}
+      style={style}
+      ref={ref}
+      {...others}
+    >
       <div className={`${prefixCls}-item-header`} onClick={onClick}>
         <div className={`${prefixCls}-item-header-label`}>{label}</div>
         <div
@@ -29,14 +47,13 @@ const CollapsePanelItem = (props: CollapsePanelItemProps) => {
           {getIcon()}
         </div>
       </div>
-      <div
-        className={`${prefixCls}-item-content`}
-        style={{ display: active ? 'block' : 'none' }}
-      >
-        {children}
-      </div>
+      <Collapse in={active} orientation="vertical" timeout={300}>
+        <div className={`${prefixCls}-item-content`}>{children}</div>
+      </Collapse>
     </div>
   );
-};
+});
+
+CollapsePanelItem.displayName = 'BuiCollapsePanelItem';
 
 export default CollapsePanelItem;
