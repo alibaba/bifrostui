776ebeb7355b22e6d5d7c01a4a8b5673c0cd5e8b
NeverEllipsis <chengkang.zhu@qq.com>
2024-11-11 12:52:09 +0800

    chore: remove usless code

--- packages/bui-core/src/ThemeProvider/hooks/createTheme.ts
+++ packages/bui-core/src/ThemeProvider/hooks/createTheme.ts
@@ -1,11 +1,9 @@
 import { ThemeProps } from '../ThemeProvider.types';
 
-// 待补充扩展
 export default function createTheme(options: ThemeProps) {
-  const { locale, responsive, ...others } = options || {};
+  const { locale, ...others } = options || {};
   const buiTheme = {
     locale,
-    responsive,
     others,
   };
 
