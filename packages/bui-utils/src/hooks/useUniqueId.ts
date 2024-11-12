00c4f2fd4d25ec6ca6a9c8f28fff00fe3f668756
NeverEllipsis <chengkang.zhu@qq.com>
2024-11-12 14:36:06 +0800

    fix: unique id

--- packages/bui-utils/src/hooks/useUniqueId.ts
+++ packages/bui-utils/src/hooks/useUniqueId.ts
@@ -1,13 +1,13 @@
 import { useRef } from 'react';
 
-const globalClientIdCounter = 0;
+let globalClientIdCounter = 0;
 
 function useUniqueId() {
   const idRef = useRef<string>();
 
   if (!idRef.current) {
-    const globalClientId = globalClientIdCounter + 1;
-    idRef.current = `b${globalClientId.toString(32)}`;
+    globalClientIdCounter += 1;
+    idRef.current = `b${globalClientIdCounter.toString(32)}`;
   }
 
   return idRef.current;
