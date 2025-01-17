969accba9868ae1465c824c1ba58e00742aba571
liudong <dong_liu123@163.com>
2025-01-17 18:02:15 +0800

    feat: DesktopPicker单测补充

--- packages/bui-core/src/DesktopPicker/__tests__/DesktopPicker.test.tsx
+++ packages/bui-core/src/DesktopPicker/__tests__/DesktopPicker.test.tsx
@@ -1,24 +1,28 @@
-import React, { act, RefObject, useState } from 'react';
-import { render, userEvent } from 'testing';
-import { DesktopPicker } from '..';
+import React from 'react';
+import { act, userEvent, render, fireEvent } from 'testing';
 
 describe('DesktopPicker', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('should render float dom', async () => {
+    const { default: DesktopPicker } = await import('../index');
+    const closeMock = jest.fn();
     const TestFn = () => {
-      const rootRef: RefObject<HTMLDivElement> = React.createRef();
-      const [open, setOpen] = useState(false);
+      const [open, setOpen] = React.useState(false);
       return (
-        <DesktopPicker
-          open={open}
-          content={<div>float</div>}
-          onClose={() => {
-            console.log('close');
-          }}
-        >
-          <div ref={rootRef}>
-            <div onClick={() => setOpen(true)}>testDemo</div>
-          </div>
-        </DesktopPicker>
+        <>
+          <DesktopPicker
+            open={open}
+            content={<div>float</div>}
+            onClose={closeMock}
+          >
+            <div>
+              <div onClick={() => setOpen(!open)}>testDemo</div>
+            </div>
+          </DesktopPicker>
+          <div>close</div>
+        </>
       );
     };
 
@@ -29,5 +33,49 @@ describe('DesktopPicker', () => {
     expect(
       document.getElementsByClassName('bui-desktop-picker-container')[0],
     ).toHaveClass('bui-desktop-picker-container-bottom');
+    await act(async () => {
+      userEvent.click(getAllByText('close')[0]);
+    });
+    expect(closeMock).toBeCalled();
+
+    await act(async () => {
+      userEvent.click(getAllByText('testDemo')[0]);
+    });
+    await act(async () => {
+      userEvent.click(getAllByText('testDemo')[0]);
+    });
+    setTimeout(() => {
+      expect(closeMock).toBeCalled();
+    }, 1000);
+  });
+});
+
+describe('prop: miniBackdropInvisible', () => {
+  const restApi = jest.requireActual('react');
+  const originalModule = jest.requireActual('@bifrostui/utils');
+  const closeMock = jest.fn();
+  it('should render float dom', async () => {
+    jest.resetModules();
+    jest.doMock('@bifrostui/utils', () => ({
+      ...originalModule,
+      isMini: true,
+    }));
+    jest.doMock('react', () => ({
+      ...restApi,
+    }));
+    const { default: DesktopPicker } = await import('../index');
+    const component = (
+      <DesktopPicker onClose={closeMock} open content={<div>test</div>}>
+        <div>content</div>
+      </DesktopPicker>
+    );
+    render(component);
+    const button = document.querySelector('.bui-backdrop');
+    await act(async () => {
+      fireEvent.click(button);
+    });
+    setTimeout(() => {
+      expect(closeMock).toBeCalled();
+    }, 1000);
   });
 });
