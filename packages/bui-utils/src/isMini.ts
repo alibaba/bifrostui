2601d133fa6e459e35321b171338bbc77de6f91f
zhubeniii <zhubeniii@163.com>
2025-02-20 17:57:48 +0800

    feat: 计算方向切换的函数适配支付宝小程序

--- packages/bui-utils/src/isMini.ts
+++ packages/bui-utils/src/isMini.ts
@@ -1,8 +1,9 @@
 const isMini = typeof process.env.TARO_ENV === 'string';
 const isWeapp = process.env.TARO_ENV === 'weapp';
 const isAlipay = process.env.TARO_ENV === 'alipay';
+const isTt = process.env.TARO_ENV === 'tt';
 
-export { isMini, isWeapp, isAlipay };
+export { isMini, isWeapp, isAlipay, isTt };
 
 export const isMiniapp = () => {
   return isMini;
