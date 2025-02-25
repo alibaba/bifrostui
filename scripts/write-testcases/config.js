609d8c04516a651dafe4952dacfec0fe8c9ca483
mingle98 <2293188960@qq.com>
2025-02-25 16:47:23 +0800

    fix: 修复代码块提取bug

--- scripts/write-testcases/config.js
+++ scripts/write-testcases/config.js
@@ -1,8 +1,8 @@
 const config = {
     // AI参考并学习的组件目录
-    referenceDirPath: '../../packages/bui-core/src/CollapsePanel',
+    referenceDirPath: '../../packages/bui-core/src/Slider',
     // AI参考并学习的组件目录下的参考测试用文件名称
-    referenceTestFileName: 'CollapsePanel.test.tsx',
+    referenceTestFileName: 'Slider.test.tsx',
     // AI参考并学习的组件目录需要过滤掉的文件后缀（或文件夹）
     referenceIgnoreDir: ['.md', '.less', '__tests__', '__test__'],
 }
