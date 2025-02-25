ca823dbfda7e77e5c03fbe5d1134bd2bd7c17c3b
mingle98 <2293188960@qq.com>
2025-02-25 15:26:11 +0800

    feat: 默认ai学习目录

--- scripts/write-testcases/config.js
+++ scripts/write-testcases/config.js
@@ -1,8 +1,8 @@
 const config = {
     // AI参考并学习的组件目录
-    referenceDirPath: '../../packages/bui-core/src/Alert',
+    referenceDirPath: '../../packages/bui-core/src/CollapsePanel',
     // AI参考并学习的组件目录下的参考测试用文件名称
-    referenceTestFileName: 'Alert.test.tsx',
+    referenceTestFileName: 'CollapsePanel.test.tsx',
     // AI参考并学习的组件目录需要过滤掉的文件后缀（或文件夹）
     referenceIgnoreDir: ['.md', '.less', '__tests__', '__test__'],
 }
