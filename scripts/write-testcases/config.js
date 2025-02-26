74581ef793c065c015dc72f2aa4e6bc7b434d5c0
mingle98 <2293188960@qq.com>
2025-02-26 15:08:15 +0800

    feat: 优化AI写单元测试工具

--- scripts/write-testcases/config.js
+++ scripts/write-testcases/config.js
@@ -5,6 +5,21 @@ const config = {
     referenceTestFileName: 'Slider.test.tsx',
     // AI参考并学习的组件目录需要过滤掉的文件后缀（或文件夹）
     referenceIgnoreDir: ['.md', '.less', '__tests__', '__test__'],
+    // 使用模型的配置
+    modelCfg: {
+      // 翻译MD使用的模型
+      translateModel: 'qwen-coder-plus',
+      // 写单元测试使用的模型（测试效果较好的模型qwen-coder-plus、deepseek-v3），更多可用模型参考：https://bailian.console.aliyun.com/?spm=5176.12818093_47.resourceCenter.6.57ea16d0lfCepy#/model-market
+      writeModel: 'qwen-coder-plus'
+    },
+    // 注意：正常使用下面的配置即可，不需要修改；除非你非常清楚你自己在做什么
+    // 需要自定义给AI加上的提示词配置
+    customPromptCfg: {
+      // 是否完全替换默认提示词（true：完全替换，false：在默认提示词基础上追加）
+      replaceDefaultPrompt: false,
+      // 自定义提示词(注意：如果默认就够满足需求，就不填)
+      prompt: ''
+    },
 }
 
 module.exports = config;
--- scripts/write-testcases/core/userPropmt.js
+++ scripts/write-testcases/core/userPropmt.js
@@ -5,7 +5,7 @@ const {getfilesContent, getTargetFile} = require('./utils.js');
 const aiTaskPrompts = {
     aiTranslateMd: {
         name: 'AI翻译markdown',
-        model: 'qwen-coder-plus',
+        model: config.modelCfg.translateModel || 'qwen-coder-plus',
         description: '将指定目录下的markdown文件翻译成英文',
         promptCfg: {
             system: '你是一个专业的翻译专家，请将以下提供的markdown翻译成英文，注意保持原文格式不变仅翻译内容，直接输出翻译后的markdown内容：',
@@ -16,8 +16,7 @@ const aiTaskPrompts = {
     },
     aiWriteTestCases: {
         name: 'AI生成测试用例',
-        model: 'qwen-coder-plus',
-        // model: 'deepseek-v3',
+        model: config.modelCfg.writeModel || 'qwen-coder-plus',
         description: '根据指定组件代码生成对应的测试用例',
         promptCfg: {
             getSystem: function () {
@@ -35,8 +34,16 @@ const aiTaskPrompts = {
             },
             getUserPrompt: function (args) {
                 // const userStr = `请为下面提供的组件代生成对应的测试用例：${args.originCode}`;
-                const userStr = `请为下面提供的组件代生成对应的测试用例（注意请确保你生成的测试用例的实现（API、方法、属性等）与该组件的实现对应且正确，不允许增加与组件实现中无关的属性或API等）：${args.originCode}`;
-                return userStr
+                const defaultPrompt = '请为下面提供的组件代生成对应的测试用例（注意请确保你生成的测试用例的实现（API、方法、属性等）与该组件的实现对应且正确，不允许增加与组件实现中无关的属性或API等）';
+                let userStr = `${defaultPrompt}：\n${args.originCode}`;
+                if (config?.customPromptCfg?.prompt) {
+                  if (config.customPromptCfg.replaceDefaultPrompt) {
+                    userStr = `${config.customPromptCfg.prompt}：\n${args.originCode}`;
+                  } else {
+                    userStr = `${defaultPrompt},\n${config.customPromptCfg.prompt}：\n${args.originCode}`
+                  };
+                };
+                return userStr;
             },
             getSnapshotTest: function (yourComponentName='yourComponentName') {
                 return `import { snapshotTest } from 'testing';
