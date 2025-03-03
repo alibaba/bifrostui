9986551b98b4b980a4b6fc96a5bd8e94534822c0
mingle98 <2293188960@qq.com>
2025-03-03 10:04:38 +0800

    feat: 优化工具

--- scripts/write-testcases/config.js
+++ scripts/write-testcases/config.js
@@ -11,7 +11,7 @@ const config = {
     modelCfg: {
       // 翻译MD使用的模型
       translateModel: 'qwen-coder-plus-latest',
-      // 写单元测试使用的模型（测试效果较好的模型qwen-max-2025-01-25、qwen-coder-plus、deepseek-v3、qwen-coder-plus-latest），更多可用模型参考：https://bailian.console.aliyun.com/?spm=5176.12818093_47.resourceCenter.6.57ea16d0lfCepy#/model-market
+      // 写单元测试使用的模型（测试效果较好的模型qwen-long、qwen-coder-plus、deepseek-v3、qwen-max），更多可用模型参考：https://bailian.console.aliyun.com/?spm=5176.12818093_47.resourceCenter.6.57ea16d0lfCepy#/model-market
       writeModel: 'qwen-coder-plus'
     },
     // ！！！注意：正常使用下面的配置即可，不需要修改；除非你非常清楚你自己在做什么
@@ -19,8 +19,6 @@ const config = {
     // ！！！注意：正常使用下面的配置即可，不需要修改；除非你非常清楚你自己在做什么
     // 需要自定义给AI加上“自动化生成测试用例”的提示词配置
     customPromptCfg: {
-      // 是否完全替换默认提示词（true：完全替换，false：在默认提示词基础上追加）
-      replaceDefaultPrompt: false,
       // 自定义提示词(注意：如果默认就够满足需求，就不填)
       prompt: ''
     },
