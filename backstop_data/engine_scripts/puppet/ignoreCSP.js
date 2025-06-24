01c2d7e3694382e5c5190ed23b219af69dc7b78d
whyrove <whyrove@163.com>
2025-06-24 17:59:18 +0800

    fix: 修复安全警告

--- backstop_data/engine_scripts/puppet/ignoreCSP.js
+++ backstop_data/engine_scripts/puppet/ignoreCSP.js
@@ -22,9 +22,7 @@
 
 const fetch = require('node-fetch');
 const https = require('https');
-const agent = new https.Agent({
-  rejectUnauthorized: false
-});
+const agent = new https.Agent({});
 
 module.exports = async function (page, scenario) {
   const intercept = async (request, targetUrl) => {
@@ -33,14 +31,16 @@ module.exports = async function (page, scenario) {
     // FIND TARGET URL REQUEST
     if (requestUrl === targetUrl) {
       const cookiesList = await page.cookies(requestUrl);
-      const cookies = cookiesList.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
+      const cookies = cookiesList
+        .map((cookie) => `${cookie.name}=${cookie.value}`)
+        .join('; ');
       const headers = Object.assign(request.headers(), { cookie: cookies });
       const options = {
         headers,
         body: request.postData(),
         method: request.method(),
         follow: 20,
-        agent
+        agent,
       };
 
       const result = await fetch(requestUrl, options);
@@ -51,7 +51,7 @@ module.exports = async function (page, scenario) {
       await request.respond({
         body: buffer,
         headers: cleanedHeaders,
-        status: result.status
+        status: result.status,
       });
     } else {
       request.continue();
@@ -59,7 +59,7 @@ module.exports = async function (page, scenario) {
   };
 
   await page.setRequestInterception(true);
-  page.on('request', req => {
+  page.on('request', (req) => {
     intercept(req, scenario.url);
   });
 };
