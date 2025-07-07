e04cab64f3a246d6c4b586a8407b894571505988
NeverEllipsis <chengkang.zhu@qq.com>
2025-07-07 18:05:26 +0800

    chore: update benchmark script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo "Please provide component name as parameter, for example: pnpm benchmark Button"
-  exit 1
+  pnpm --filter @bifrostui/react build:umd
+  echo "🚀 The main branch control package has been generated, please go to the target branch for benchmark testing."
+  exit 0
 fi
 
 COMPONENT=$1
 SUBPACKAGE="packages/bui-core"
+UMD_DIR="${SUBPACKAGE}/umd"
+UMD_MAIN_DIR="${SUBPACKAGE}/umd-main"
+
+if [ ! -d "$UMD_DIR" ]; then
+  echo "❌ Error: $UMD_DIR The directory does not exist. Please build the UMD product first."
+  exit 1
+fi
+
+if [ ! -d "$UMD_MAIN_DIR" ]; then
+  echo "❌ Error: $UMD_MAIN_DIR The directory does not exist. Please ensure that the main branch control package has been generated."
+  exit 1
+fi
 
 pnpm --filter @bifrostui/styles build
 pnpm --filter @bifrostui/react build:umd
