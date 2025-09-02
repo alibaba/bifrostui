25119080fd9121e2be8ce0579a36f73957b32708
lawpachi <15802994779@163.com>
2025-09-02 11:34:12 +0800

    Merge remote-tracking branch 'origin/releases/pioneer' into releases/theme-next

--- packages/bui-styles/registry/pioneer-light.mjs
+++ packages/bui-styles/registry/pioneer-light.mjs
@@ -7,36 +7,82 @@ export const pioneerLight = {
     '--bui-font-family-accent':
       "'AlibabaPuHuiTi-Heavy', 'PingFang SC', 'Chinese Quote', 'Segoe UI', roboto, 'Hiragino Sans GB', 'Microsoft YaHei', 'Helvetica Neue', helvetica, arial, sans-serif",
 
-    /** 语义色 */
-    '--bui-color-primary': '#ff7600',
-    '--bui-color-primary-light': '#ffefe5',
+    /** 品牌红 */
+    '--bui-color-red': '#ff471a',
+    '--bui-color-red-light': '#ffefe6',
+    '--bui-color-red-dark': '#b31800',
+
+    /** 活力橙 */
+    '--bui-color-orange': '#ff5000',
+    '--bui-color-orange-light': '#ffede5',
+    '--bui-color-orange-dark': '#d93d00',
+
+    /** 活力黄 */
+    '--bui-color-yellow': '#ffa608',
+    '--bui-color-yellow-light': '#fffae6',
+    '--bui-color-yellow-dark': '#b36500',
+
+    /** 荧光黄 */
+    '--bui-color-lemonyellow': '#fff36f',
+    '--bui-color-lemonyellow-light': '#ffffeb',
+    '--bui-color-lemonyellow-dark': '#b3a13d',
+
+    /** 亲和绿 */
+    '--bui-color-green': '#40bf00',
+    '--bui-color-green-light': '#f2ffe6',
+    '--bui-color-green-dark': '#1f7300',
+
+    /** 神秘紫 */
+    '--bui-color-purple': '#754dff',
+    '--bui-color-purple-light': '#f6f0ff',
+    '--bui-color-purple-dark': '#3924b3',
+
+    /** 宁静蓝 */
+    '--bui-color-blue': '#229af0',
+    '--bui-color-blue-light': '#f0fbff',
+    '--bui-color-blue-dark': '#0757a3',
+
+    /** 智能蓝 */
+    '--bui-color-aiblue': '#5cfffa',
+    '--bui-color-aiblue-light': '#f0fffc',
+    '--bui-color-aiblue-dark': '#1d858c',
+
+    /** 主题 */
+    '--bui-color-primary': 'var(--bui-color-orange)',
+    '--bui-color-primary-light': 'var(--bui-color-orange-light)',
+    '--bui-color-primary-dark': 'var(--bui-color-orange-dark)',
 
     /** 语义化字号 */
-    '--bui-title-size-4': '14px',
+    '--bui-title-size-4': 'var(--bui-font-size-md1)',
 
-    // 前景色，通常用于文字
-    // default是最通用的色彩，比如标题和一级信息
-    '--bui-color-fg-default': '#181818',
     // 用于失效、不可用等状态
-    '--bui-color-fg-disabled': 'rgba(24, 24, 24, 0.5)',
+    '--bui-color-fg-disabled': 'rgba(3, 11, 26, 0.5)',
 
-    // 分割线、边框
-    '--bui-color-border-primary': 'rgba(20, 138, 255, 0.4)',
+    /** 分割线、边框 */
+    '--bui-color-border-primary': 'rgba(255, 80, 0, 0.4)',
+    '--bui-color-border-info': 'rgba(34, 154, 240, 0.4)',
+    '--bui-color-border-success': 'rgba(64, 191, 0, 0.4)',
+    '--bui-color-border-warning': 'rgba(255, 80, 0, 0.4)',
+    '--bui-color-border-danger': 'rgba(255, 71, 26, 0.4)',
 
-    // 主题色渐变
-    '--bui-color-primary-start': '#ff9c0d',
-    '--bui-color-primary-end': '#ff6813',
-    '--bui-color-info-start': '#17b6ff',
-    '--bui-color-info-end': '#48d3ff',
-    '--bui-color-warning-start': '#ffcf02',
-    '--bui-color-warning-end': '#ffb205',
+    /** 主题色渐变 */
+    '--bui-color-primary-start': '#ffb300',
+    '--bui-color-primary-end': 'var(--bui-color-primary)',
+    '--bui-color-info-start': '#5bc8ff',
+    '--bui-color-info-end': 'var(--bui-color-info)',
+    '--bui-color-warning-start': '#fc3',
+    '--bui-color-warning-end': 'var(--bui-color-warning)',
+    '--bui-color-success-start': '#66d933',
+    '--bui-color-success-end': 'var(--bui-color-success)',
+    '--bui-color-danger-start': '#ff7633',
+    '--bui-color-danger-end': 'var(--bui-color-danger)',
 
-    // 形状定义
-    '--bui-shape-radius-drawer': '15px',
+    /** 形状定义 */
+    '--bui-shape-radius-drawer': 'var(--bui-radius-6)',
     // 卡片、普通视图、抽屉、action sheet，常用于信息密度较低的视图
-    '--bui-shape-radius-card': '12px',
+    '--bui-shape-radius-card': 'var(--bui-radius-5)',
     // 独立海报、卡片，常用于信息密度较高的视图
-    '--bui-shape-radius-default': '9px',
+    '--bui-shape-radius-default': 'var(--bui-radius-4)',
 
     // Button
     '--bui-btn-full-height': '45px',
