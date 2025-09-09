a63462f626f9fd1d13f4416062213067612d74b0
garyzhangmin1 <zm5698@126.com>
2025-09-09 17:21:05 +0800

    fix: (TextArea)调整文本区域自动高度计算公式
    修复了文本区域高度计算中的一个小错误，移除了不必要的偏移量，使高度计算更准确。
    
    Co-developed-by: Aone Copilot <noreply@alibaba-inc.com>

--- packages/bui-core/src/TextArea/TextArea.tsx
+++ packages/bui-core/src/TextArea/TextArea.tsx
@@ -91,7 +91,7 @@ const TextArea = React.forwardRef<HTMLDivElement, TextAreaProps>(
       const { height, lineCount } = e?.detail || {};
       const line = lineCount <= 1 ? rows : lineCount;
       // 总高度 = 行数 * 单行高度
-      const textAreaHeight = handleAutoHeight(line * (height / lineCount) + 4);
+      const textAreaHeight = handleAutoHeight(line * (height / lineCount));
       // autoSize=false也需要初始化小程序textarea高度
       if (!initLock.current && !autoSize && rows === DEFAULT_ROWS) {
         textArea.style.height = `${textAreaHeight}px`;
