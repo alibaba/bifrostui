42b35ac2e59c26266da9588f7f0d33d7abaab2f9
youhe <1003837059@qq.com>
2025-09-11 16:46:03 +0800

    fix: 修复container无效以及ref无效的问题

--- packages/bui-core/src/Toast/Toast.tsx
+++ packages/bui-core/src/Toast/Toast.tsx
@@ -12,84 +12,85 @@ import './index.less';
 
 const prefixCls = 'bui-toast';
 
-const ToastComponent = React.forwardRef<HTMLDivElement, ToastProps>(
-  (props, ref) => {
-    const {
-      className,
-      style,
-      open,
-      type,
-      icon,
-      message,
-      position = 'center',
-      disableClick = false,
-      ...others
-    } = props;
+const ToastComponent = React.forwardRef<
+  HTMLDivElement,
+  Omit<ToastProps, 'container'>
+>((props, ref) => {
+  const {
+    className,
+    style,
+    open,
+    type,
+    icon,
+    message,
+    position = 'center',
+    disableClick = false,
+    ...others
+  } = props;
 
-    const iconMap = {
-      fail: (
-        <ErrorCircleFilledBoldIcon
-          htmlColor="var(--bui-color-white)"
-          aria-hidden="true"
-        />
-      ),
-      loading: (
-        <AccessTimeCircleFilledBoldIcon
-          htmlColor="var(--bui-color-white)"
-          aria-hidden="true"
-        />
-      ),
-      warning: (
-        <AlertCircleFilledBoldIcon
-          htmlColor="var(--bui-color-white)"
-          aria-hidden="true"
-        />
-      ),
-      success: (
-        <SuccessCircleFilledBoldIcon
-          htmlColor="var(--bui-color-white)"
-          aria-hidden="true"
-        />
-      ),
-    };
-    const iconDom = iconMap[type] || icon;
+  const iconMap = {
+    fail: (
+      <ErrorCircleFilledBoldIcon
+        htmlColor="var(--bui-color-white)"
+        aria-hidden="true"
+      />
+    ),
+    loading: (
+      <AccessTimeCircleFilledBoldIcon
+        htmlColor="var(--bui-color-white)"
+        aria-hidden="true"
+      />
+    ),
+    warning: (
+      <AlertCircleFilledBoldIcon
+        htmlColor="var(--bui-color-white)"
+        aria-hidden="true"
+      />
+    ),
+    success: (
+      <SuccessCircleFilledBoldIcon
+        htmlColor="var(--bui-color-white)"
+        aria-hidden="true"
+      />
+    ),
+  };
+  const iconDom = iconMap[type] || icon;
 
-    // 根据类型设置无障碍role和aria-live
-    let role: 'status' | 'alert' = 'status';
-    let ariaLive: 'polite' | 'assertive' = 'polite';
-    if (type === 'fail' || type === 'warning') {
-      role = 'alert';
-      ariaLive = 'assertive';
-    }
+  // 根据类型设置无障碍role和aria-live
+  let role: 'status' | 'alert' = 'status';
+  let ariaLive: 'polite' | 'assertive' = 'polite';
+  if (type === 'fail' || type === 'warning') {
+    role = 'alert';
+    ariaLive = 'assertive';
+  }
 
-    return (
-      <>
-        <div className={clsx({ [`${prefixCls}-backdrop`]: disableClick })} />
-        <Fade {...others} in={open} appear={false} unmountOnExit>
-          <div
-            ref={ref}
-            className={clsx(
-              prefixCls,
-              `${prefixCls}-${position}`,
-              {
-                [`${prefixCls}-icon`]: !!iconDom,
-                [`${prefixCls}-allow-click`]: !disableClick,
-              },
-              className,
-            )}
-            style={style}
-            role={role}
-            aria-live={ariaLive}
-            aria-atomic="true"
-          >
-            {iconDom}
-            {message}
-          </div>
-        </Fade>
-      </>
-    );
-  },
-);
+  return (
+    <>
+      <div className={clsx({ [`${prefixCls}-backdrop`]: disableClick })} />
+      <Fade {...others} in={open} appear={false} unmountOnExit>
+        <div
+          ref={ref}
+          className={clsx(
+            prefixCls,
+            `${prefixCls}-${position}`,
+            {
+              [`${prefixCls}-icon`]: !!iconDom,
+              [`${prefixCls}-allow-click`]: !disableClick,
+            },
+            className,
+          )}
+          style={style}
+          role={role}
+          aria-live={ariaLive}
+          aria-atomic="true"
+        >
+          {iconDom}
+          {message}
+        </div>
+      </Fade>
+    </>
+  );
+});
 
 ToastComponent.displayName = 'BuiToast';
 
