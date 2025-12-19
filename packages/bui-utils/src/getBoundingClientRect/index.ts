ab2071c730a22b20d6522b390f787f4f9392e6b5
lawpachi <15802994779@163.com>
2025-12-19 10:25:03 +0800

    fix: update getBoundingClientRect for null element handling

--- packages/bui-utils/src/getBoundingClientRect/index.ts
+++ packages/bui-utils/src/getBoundingClientRect/index.ts
@@ -3,17 +3,7 @@ export default function getBoundingClientRect(
 ): Promise<DOMRect> {
   if (!ele) {
     // Return a default DOMRect when element is null
-    return Promise.resolve({
-      width: 0,
-      height: 0,
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      x: 0,
-      y: 0,
-      toJSON: () => ({}),
-    } as DOMRect);
+    return Promise.resolve(null);
   }
   return Promise.resolve(ele.getBoundingClientRect());
 }
