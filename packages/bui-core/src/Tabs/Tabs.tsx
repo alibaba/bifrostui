b5d5a707c7baf15616bb1af954dc28733a9bf8a5
youhe <1003837059@qq.com>
2026-01-27 17:19:06 +0800

    feat: 修复Tabs在1个tab的情况下展示mask元素

--- packages/bui-core/src/Tabs/Tabs.tsx
+++ packages/bui-core/src/Tabs/Tabs.tsx
@@ -124,7 +124,7 @@ const Tabs = React.forwardRef<HTMLDivElement, TabsProps>((props, ref) => {
 
   return (
     <div className={clsx(tabsRootClass, className)} {...others} ref={ref}>
-      {isScrollable && (
+      {isScrollable && tabs.length > 1 && (
         <>
           <TabMask position="left" />
           <TabMask position="right" />
